714b1e032c547cdc3602bf2fe7c3c6ff2b2f9608
Frank Wilson <pupinc@hotmail.com>
2025-04-24 19:48:56 -0400

    Added self generating docs

--- src/paddocs/genHTML.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pandoc -s --toc --number-sections --highlight-style=pygments -c "$dir/templates/jez.css" --template "$dir/templates/jez.html" --metadata pagetitle="${cmd}" $save_dir/${cmd}.md -o "$save_dir/${cmd}.html"
-[[ $? -eq 0 ]] && printf " :: Generated: $cmd.html\n"
--- src/paddocs/genPDF.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pandoc --toc --pdf-engine=xelatex --metadata pagetitle="$cmd" "$save_dir/${cmd}.md" -o "$save_dir/${cmd}.pdf"
-[[ $? -eq 0 ]] && printf " :: Generated $cmd.pdf\n"
